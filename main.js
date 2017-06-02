cc6df020d3381d00e1c22742882012aa1be99a03
Samadi van Koten <me@vktec.co.uk>
2017-06-02 14:38:48 +0100

    Switch to async loading of tracks.json using promises

--- main.js
+++ main.js
@@ -1,41 +1,54 @@
 (function () {
-  // AJAX, minus the A and the X
-  var tracks = (function (path) {
-    req = new XMLHttpRequest();
-    req.open("GET", path, false); // false for synchronous, hence no callback
-    req.send();
+  var loadJson = function (path) {
+    return new Promise(function (res, rej) {
+      var req = new XMLHttpRequest();
 
-    if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
-      // Success
-      return JSON.parse(req.responseText);
-    }
-  })("tracks.json");
+      req.onload = function () {
+        if (req.status === 200) {
+          res(JSON.parse(req.responseText));
+        } else {
+          rej(Error("Failed to load JSON; HTTP status code: " + request.statusText));
+        }
+      };
 
-  var app = new Vue({
-    el: "#app",
-    data: {
-      tracks: tracks,
-      current: 0,
-      playing: false,
-    },
-    methods: {
-      log: console.log,
-      load: function () {
-        if (this.playing) {
-          document.getElementById("player").play();
+      req.onerror = function () {
+        rej(Error("Failed to load JSON; network error"))
+      };
+
+      req.open("GET", path);
+      req.send();
+    });
+  };
+
+  loadJson("tracks.json").then(function (tracks) {
+    var app = new Vue({
+      el: "#app",
+      data: {
+        tracks: tracks,
+        current: 0,
+        playing: false,
+      },
+      methods: {
+        log: console.log,
+        load: function () {
+          if (this.playing) {
+            document.getElementById("player").play();
+          }
+        },
+        next: function () { this.select(this.current + 1); },
+        prev: function () { this.select(this.current + 1); },
+        select: function (index) {
+          if (index >= 0 && index < this.tracks.length) {
+            this.playing = true;
+            this.current = index;
+            var player = document.getElementById("player");
+            player.currentTime = 0;
+            player.play();
+          }
         }
       },
-      next: function () { this.select(this.current + 1); },
-      prev: function () { this.select(this.current + 1); },
-      select: function (index) {
-        if (index >= 0 && index < this.tracks.length) {
-          this.playing = true;
-          this.current = index;
-          var player = document.getElementById("player");
-          player.currentTime = 0;
-          player.play();
-        }
-      }
-    },
+    });
+  }, function (err) {
+    console.log(err);
   });
 }).call(window);
